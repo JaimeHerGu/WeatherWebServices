ac4971002f880da186209edbd517a7ecf71fd0d1
JaimeHerGu <60728406+JaimeHerGu@users.noreply.github.com>
2020-02-08 14:37:49 -0600

    Add files via upload
    FirstCommit

---
+++ Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WebServiceWeather
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "2ea1e9a2c3dfd4986bb601fe93b32243";
+    }
+}
